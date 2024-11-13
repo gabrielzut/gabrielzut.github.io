8ff6b95ff59e1860e3b6776d01e6b395e98fcb4d
Gabriel Augusto Zutião <gabriel_zut@yahoo.com.br>
2024-11-13 19:02:50 -0300

    WIP - File explorer

--- src/components/programs/FileExplorer.tsx
+++ src/components/programs/FileExplorer.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useMemo, useState } from "react";
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { ProgramEntry } from ".";
 import folderIcon from "../../assets/img/folder.gif";
 import { WindowOptions } from "../general/WindowOption";
@@ -16,13 +16,26 @@ interface FileExplorerProps {
 
 const HOME_PATH = ["home", "user"];
 
-interface FileEntryProps {
-  file: GeneralFile;
+interface ShortcutEntryProps {
+  shortcut: { name: string; icon: string };
 }
 
-const FileEntry: FC<FileEntryProps> = ({ file }) => {
+const ShortcutEntry: FC<ShortcutEntryProps> = ({ shortcut }) => {
+  return <div className="shortcut-entry">{shortcut.name}</div>;
+};
+
+interface FileEntryProps {
+  file: GeneralFile;
+  onClick: (event: React.MouseEvent) => void;
+  selected: boolean;
+}
+
+const FileEntry: FC<FileEntryProps> = ({ file, onClick, selected }) => {
   return (
-    <div className="file-entry">
+    <div
+      className={`file-entry ${selected ? "selected" : ""}`}
+      onClick={onClick}
+    >
       <div className="icon">
         {
           <img
@@ -36,6 +49,15 @@ const FileEntry: FC<FileEntryProps> = ({ file }) => {
   );
 };
 
+const userShortcuts = [
+  { name: "Documents", icon: "" },
+  { name: "Downloads", icon: "" },
+  { name: "Images", icon: "" },
+  { name: "Music", icon: "" },
+  { name: "Public", icon: "" },
+  { name: "Videos", icon: "" },
+];
+
 export const FileExplorer: FC<FileExplorerProps> = ({
   startingPath = HOME_PATH,
 }) => {
@@ -47,10 +69,39 @@ export const FileExplorer: FC<FileExplorerProps> = ({
   const [backStack, setBackStack] = useState<string[][]>([startingPath]);
   const [forwardStack, setForwardStack] = useState<string[][]>([]);
   const root = useSelector((state: RootState) => state.fileSystem.root);
+  const [selectedFiles, setSelectedFiles] = useState<string[]>([]);
+  const [lastSelectedIndex, setLastSelectedIndex] = useState<number | null>(
+    null
+  );
   const currentDirectory = useMemo(
     () => findFolder(root as Folder, path),
     [path, root]
   );
+  const fileListRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setSelectedFiles([]);
+  }, [path]);
+
+  const handleClickOutside = useCallback((event: MouseEvent) => {
+    const isClickOutsideFileList =
+      fileListRef.current &&
+      !fileListRef.current.contains(event.target as Node);
+    const isClickInsideContent =
+      contentRef.current && contentRef.current.contains(event.target as Node);
+
+    if (isClickOutsideFileList && isClickInsideContent) {
+      setSelectedFiles([]);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleClickOutside]);
 
   const onChangePathInputValue = useCallback(
     (value: string[]) => setPathInputValue(`/${value.join("/")}`),
@@ -65,6 +116,36 @@ export const FileExplorer: FC<FileExplorerProps> = ({
       setForwardStack([]);
     },
     [onChangePathInputValue, path]
+  );
+
+  const handleSelectFiles = useCallback(
+    (index: number, fileName: string, event: React.MouseEvent) => {
+      if (event.detail === 2) {
+        updatePath([...path, fileName]);
+      }
+
+      if (event.shiftKey && lastSelectedIndex !== null) {
+        const start = Math.min(lastSelectedIndex, index);
+        const end = Math.max(lastSelectedIndex, index);
+        const range = currentDirectory?.files
+          .slice(start, end + 1)
+          .map((file) => file.name);
+        setSelectedFiles((prevSelected) =>
+          Array.from(new Set([...prevSelected, ...(range ?? [])]))
+        );
+      } else if (event.ctrlKey || event.metaKey) {
+        setSelectedFiles((prevSelected) =>
+          prevSelected.includes(fileName)
+            ? prevSelected.filter((id) => id !== fileName)
+            : [...prevSelected, fileName]
+        );
+        setLastSelectedIndex(index);
+      } else {
+        setSelectedFiles([fileName]);
+        setLastSelectedIndex(index);
+      }
+    },
+    [currentDirectory?.files, lastSelectedIndex, path, updatePath]
   );
 
   const handleGoBack = useCallback(() => {
@@ -148,6 +229,7 @@ export const FileExplorer: FC<FileExplorerProps> = ({
         >
           <img className="icon" src={homeIcon} alt="Home icon" />
         </button>
+        <div className="divider" />
         <input
           value={pathInputValue}
           onChange={useCallback(
@@ -165,11 +247,21 @@ export const FileExplorer: FC<FileExplorerProps> = ({
           />
         </button>
       </div>
-      <div className="content">
-        <div className="left-panel"></div>
-        <div className="file-list">
-          {currentDirectory?.files.map((file) => (
-            <FileEntry key={file.name} file={file} />
+      <div className="content" ref={contentRef}>
+        <div className="left-panel">
+          <div className="title">Shortcuts</div>
+          {userShortcuts.map((shortcut) => (
+            <ShortcutEntry shortcut={shortcut} key={shortcut.name} />
+          ))}
+        </div>
+        <div className="file-list" ref={fileListRef}>
+          {currentDirectory?.files.map((file, index) => (
+            <FileEntry
+              onClick={(e) => handleSelectFiles(index, file.name, e)}
+              key={file.name}
+              file={file}
+              selected={selectedFiles.includes(file.name)}
+            />
           ))}
         </div>
       </div>
